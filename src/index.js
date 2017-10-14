877f2a822812c849b85d7466fb3703a5af287b83
Schenng <scheng365@gmail.com>
2017-10-14 13:42:26 -0400

    Commented code

--- src/index.js
+++ src/index.js
@@ -25,41 +25,50 @@ const zeroEx = new ZeroEx(provider);
 	const ZRX_ADDRESS  = await zeroEx.exchange.getZRXTokenAddressAsync();        // The ZRX token contract
 	const EXCHANGE_ADDRESS   = await zeroEx.exchange.getContractAddressAsync();  // The Exchange.sol address (0x exchange smart contract)
 
-	// Getting list of accounts
+	// Getting list of accounts of the provider RPC
 	const accounts =  await zeroEx.getAvailableAddressesAsync();
 	console.log(accounts);
 
-	// Set our address
-	const [makerAddress, takerAddress] = accounts;
+	// Set maker address
+	const makerAddress = accounts[0];
+	console.log("Maker Address" + makerAddress);
 
-	// Unlimited allowances to 0x contract for maker and taker
+	//Set Taker 
+	const takerAddress = accounts[1];
+	console.log("Taker Address" + takerAddress);
+
+	// Unlimited allowances to 0x contract for maker
 	const txHashAllowMaker = await zeroEx.token.setUnlimitedProxyAllowanceAsync(ZRX_ADDRESS,  makerAddress); 
 	await zeroEx.awaitTransactionMinedAsync(txHashAllowMaker);
 
+	// Unlimited allowances to 0x contract for taker
 	const txHashAllowTaker = await zeroEx.token.setUnlimitedProxyAllowanceAsync(WETH_ADDRESS, takerAddress);
 	await zeroEx.awaitTransactionMinedAsync(txHashAllowTaker);
 
-	// Deposit WETH
-	const ethToConvert = ZeroEx.toBaseUnitAmount(new BigNumber(1), DECIMALS); // Number of ETH to convert to WETH
+	// Convert 1 to base unit 16
+	const ethToConvert = ZeroEx.toBaseUnitAmount(new BigNumber(1), DECIMALS); 
 
+	// Change ETH to WETH
 	const txHashWETH = await zeroEx.etherToken.depositAsync(ethToConvert, takerAddress);
+
+	//Mine the transaction that converts ETH to WETH
 	await zeroEx.awaitTransactionMinedAsync(txHashWETH);
 
 	// Generate order
 	const order = { 
-			 maker: makerAddress, 
-			 taker: NULL_ADDRESS,
-	           	 feeRecipient: NULL_ADDRESS,
-	           	 makerTokenAddress: ZRX_ADDRESS,
-	              	 takerTokenAddress: WETH_ADDRESS,
-	              	 exchangeContractAddress: EXCHANGE_ADDRESS,
-	              	 salt: ZeroEx.generatePseudoRandomSalt(),
-	           	 makerFee: new BigNumber(0),
-	              	 takerFee: new BigNumber(0),
-	            	 makerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.2), DECIMALS),  // Base 18 decimals
-	           	 takerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.3), DECIMALS),  // Base 18 decimals
-	           	 expirationUnixTimestampSec: new BigNumber(Date.now() + 3600000),          // Valid up to an hour
-	              };
+		maker: makerAddress, 
+		taker: NULL_ADDRESS,
+		feeRecipient: NULL_ADDRESS,
+		makerTokenAddress: ZRX_ADDRESS,
+		takerTokenAddress: WETH_ADDRESS,
+		exchangeContractAddress: EXCHANGE_ADDRESS,
+		salt: ZeroEx.generatePseudoRandomSalt(),
+		makerFee: new BigNumber(0),
+		takerFee: new BigNumber(0),
+		makerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.2), DECIMALS),  // Base 18 decimals
+		takerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.3), DECIMALS),  // Base 18 decimals
+		expirationUnixTimestampSec: new BigNumber(Date.now() + 3600000),          // Valid up to an hour
+	};
 
 	// Create orderHash
 	const orderHash = ZeroEx.getOrderHashHex(order);
@@ -72,11 +81,12 @@ const zeroEx = new ZeroEx(provider);
 			       ...order, 
 			       ecSignature, 
 	                    };
-
+	console.log("Signed Order:");
+	console.log(order);
 	// Verify if order is fillable
 	await zeroEx.exchange.validateOrderFillableOrThrowAsync(signedOrder);
 
-	// Try to fill order
+	//Try to fill order
 	const shouldThrowOnInsufficientBalanceOrAllowance = true;
 	const fillTakerTokenAmount = ZeroEx.toBaseUnitAmount(new BigNumber(0.1), DECIMALS);
 
