c0541589f8c763115c0a06c661ad925e8c0c6fc5
Schenng <scheng365@gmail.com>
2017-10-14 08:22:13 -0400

    Libs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Web3 from 'web3';
+import {ZeroEx} from '0x.js';
+import BigNumber from 'bignumber.js';
+
+// Default provider for TestRPC
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+console.log(provider)
+
+// Calling constructor
+const zeroEx = new ZeroEx(provider);
+console.log(zeroEx);	
+
+//Number of decimals to use (for ETH and ZRX)
+const DECIMALS = 18;
+
+// Addresses
+const NULL_ADDRESS = ZeroEx.NULL_ADDRESS;                              
+const WETH_ADDRESS = zeroEx.etherToken.getContractAddressAsync();  	 
+const ZRX_ADDRESS  = zeroEx.exchange.getZRXTokenAddressAsync();    	 
+const EXCHANGE_ADDRESS = zeroEx.exchange.getContractAddressAsync(); 
+
+const accounts =  zeroEx.getAvailableAddressesAsync().then(function() {
+	
+
+})
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
